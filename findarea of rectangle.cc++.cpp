a837c9c4aa036c19f13baa2bee46f53f23fcb06b
jayubha2111 <jayrajsinhjadav261@gmail.com>
2023-08-01 22:53:44 +0530

    data

---
+++ findarea of rectangle.cc++.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+int main()
+{ 
+   int 1,w,;
+   printf("enter lenght:");
+   scanf("%d",&1);
+   printf("enter width:");
+   scanf("%d",&w);
+   a=1*w;
+   printf("area is:%d",a);
+   return 0;
+}
