635c61c350f4c155e316acee208df8217e54050e
jayubha2111 <jayrajsinhjadav261@gmail.com>
2023-08-07 23:08:05 +0530

    data

---
+++ rectangle.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int l,w,a;
+	printf("enter lenght:");
+	scanf("%d",&l);
+	printf("enter width:");
+	scanf("%d",&w);
+	a=l*w;
+	printf("area is%d",a);
+	return 0;
+	
+}
