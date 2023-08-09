94b61554b11ea45981e4a3268db990c23abdc7b0
jayubha2111 <jayrajsinhjadav261@gmail.com>
2023-08-09 21:23:47 +0530

    data

---
+++ persol information.cc++.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("\t\t\tpersnol information\n");
+	printf("\t\tName:jayrajsinh jadav\n");
+	printf("\t\tAdderss:halavad road\n");
+	printf("\t\tNumber:9157783727\n");
+	printf("\t\tEamil:jayrajsinhjadav261@gamil.com\n");	
+	return 0;
+}
