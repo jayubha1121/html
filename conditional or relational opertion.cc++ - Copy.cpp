793525c2a590cec6832c652ca6caf5f240de907b
jayubha2111 <jayrajsinhjadav261@gmail.com>
2023-08-14 21:33:24 +0530

    data

---
+++ conditional or relational opertion.cc++ - Copy.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>	
+main()
+{
+//	int x=10,y=20;
+////	printf("%d",x==y);
+////	printf("%d",x>y);
+////	printf("%d",x<y);
+////	printf("%d",x>=y);
+//	printf("%d",x!=y);	
+
+
+////	logical AND
+int x=10;
+//printf("%d",x>5 && x<20);
+//printf("%d",x>20 || x<40);
+
+   printf("%d",!x>5 && x<30);
+}
