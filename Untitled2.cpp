f70847ab46a4934dbc7bec950f87db17d2973aca
jayubha2111 <jayrajsinhjadav261@gmail.com>
2023-08-17 15:38:05 +0530

    data

---
+++ Untitled2.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+	int a=100,b=200;
+	if (a==10)
+	{
+		if(b==30000)
+		{
+			printf("The value of A is 100 and B is 200");
+			
+		}
+		else{
+			printf("Inside else statement");
+		}
+	}
+	
+	else{
+		printf("Outside else statement");
+	}
+	
+}
