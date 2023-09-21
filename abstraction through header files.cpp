eb4d2b0f3e945bf7d934888bbc427d9a318f0afc
jayubha2111 <jayrajsinhjadav261@gmail.com>
2023-09-21 23:22:33 +0530

    module-1

---
+++ abstraction through header files.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+main()
+{
+	cout<<pow(2,5);
+	cout<<"\n"<<sqrt<<(64);
+	cout<<"\n"<<sin<<(78);
+	cout<<"\n"<<cos<<(65);
+	cout<<"\n"<<tan<<(54);
+	cout<<"\n"<<log<<(30);
+	cout<<"\n"<<ceil<<(78.67);
+	cout<<"\n"<<floor<<(80.76);
+}
